8889f8851e6415f2ae995c86bd47ef824a63bc6e
alexa-whitney <alexawhitney@gmail.com>
2023-10-02 12:26:59 -0600

    Challenge 6 Completed

--- index.js
+++ index.js
@@ -1,34 +1,37 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable camelcase */
+Object.defineProperty(exports, '__esModule', { value: true });
+// eslint-disable-next-line no-void
 exports.formatPhoneNumber = void 0;
+const string_o_matic_1 = require('string-o-matic');
+const D = require('ticktalk');
+const data = require('./data.json');
 // Challenge 0: Import the data.json file into this file
-var data = require('./data.json');
-// Challenge 1: Print the first_name and last_name and convert the first letter of each name to uppercase
-var string_o_matic_1 = require("string-o-matic");
-console.log((0, string_o_matic_1.capitalize)(data[0].first_name) + ' ' + (0, string_o_matic_1.capitalize)(data[0].last_name));
+// see above
+// Challenge 1: Print the first_name and last_name and convert the first letter to uppercase
+console.log(''.concat((0, string_o_matic_1.capitalize)(data[0].first_name), ' ').concat((0, string_o_matic_1.capitalize)(data[0].last_name)));
 // Challenge 2: Print the purchased date to a date and format it as: Month Day, Year
-var D = require('ticktalk');
-var purchasedDate = new D(data[0].purchased);
-console.log('Purchased: ' + purchasedDate.format('M d, Y'));
+const purchasedDate = new D(data[0].purchased);
+console.log('Purchased: '.concat(purchasedDate.format('M d, Y')));
 // Challenge 3: Print out last payment showing the date as "when". Ex. 'Last Payment: 2 days ago'
-var lastPaymentDate = new D(data[0].lastpayment);
-console.log('Last Payment: ' + lastPaymentDate.when());
+const lastPaymentDate = new D(data[0].lastpayment);
+console.log('Last Payment: '.concat(lastPaymentDate.when()));
 // Challenge 4: Format the phone number as: (xxx) xxx-xxxx
 function formatPhoneNumber(input) {
-    // Convert to string in case it's a number
-    var str = String(input);
-    // Check if input has exactly 10 characters/numbers
-    if (str.length !== 10 || !/^\d{10}$/.test(str)) {
-        throw new Error("Invalid phone number input");
-    }
-    // Extract the area code, central office code, and line number
-    var areaCode = str.substring(0, 3);
-    var centralOfficeCode = str.substring(3, 6);
-    var lineNumber = str.substring(6, 10);
-    // Return the formatted phone number
-    return "(".concat(areaCode, ") ").concat(centralOfficeCode, "-").concat(lineNumber);
+  // Convert to string in case it's a number
+  const str = String(input);
+  // Check if input has exactly 10 characters/numbers
+  if (str.length !== 10 || !/^\d{10}$/.test(str)) {
+    throw new Error('Invalid phone number input');
+  }
+  // Extract the area code, central office code, and line number
+  const areaCode = str.substring(0, 3);
+  const centralOfficeCode = str.substring(3, 6);
+  const lineNumber = str.substring(6, 10);
+  // Return the formatted phone number
+  return '('.concat(areaCode, ') ').concat(centralOfficeCode, '-').concat(lineNumber);
 }
 exports.formatPhoneNumber = formatPhoneNumber;
 // Test the function
-var phoneNumber = data[0].phone;
+const phoneNumber = data[0].phone;
 console.log('Phone Number:', formatPhoneNumber(phoneNumber));
--- index.ts
+++ index.ts
@@ -1,40 +1,43 @@
-// Challenge 0: Import the data.json file into this file
+import { capitalize } from 'string-o-matic';
+
+const D = require('ticktalk');
 const data = require('./data.json');
 
-// Challenge 1: Print the first_name and last_name and convert the first letter of each name to uppercase
-import { capitalize } from 'string-o-matic';
-console.log(capitalize(data[0].first_name) + ' ' + capitalize(data[0].last_name));
+// Challenge 0: Import the data.json file into this file
+// see above
+
+// Challenge 1: Print the first_name and last_name and convert the first letter to uppercase
+
+console.log(`${capitalize(data[0].first_name)} ${capitalize(data[0].last_name)}`);
 
 // Challenge 2: Print the purchased date to a date and format it as: Month Day, Year
-const D = require('ticktalk');
 
 const purchasedDate = new D(data[0].purchased);
-console.log('Purchased: ' + purchasedDate.format('M d, Y'));
+console.log(`Purchased: ${purchasedDate.format('M d, Y')}`);
 
 // Challenge 3: Print out last payment showing the date as "when". Ex. 'Last Payment: 2 days ago'
 const lastPaymentDate = new D(data[0].lastpayment);
-console.log('Last Payment: ' + lastPaymentDate.when());
+console.log(`Last Payment: ${lastPaymentDate.when()}`);
 
 // Challenge 4: Format the phone number as: (xxx) xxx-xxxx
-export function formatPhoneNumber(input: string | number): string {
-    // Convert to string in case it's a number
-    const str = String(input);
+export default function formatPhoneNumber(input: string | number): string {
+  // Convert to string in case it's a number
+  const str = String(input);
 
-    // Check if input has exactly 10 characters/numbers
-    if (str.length !== 10 || !/^\d{10}$/.test(str)) {
-        throw new Error("Invalid phone number input");
-    }
+  // Check if input has exactly 10 characters/numbers
+  if (str.length !== 10 || !/^\d{10}$/.test(str)) {
+    throw new Error('Invalid phone number input');
+  }
 
-    // Extract the area code, central office code, and line number
-    const areaCode = str.substring(0, 3);
-    const centralOfficeCode = str.substring(3, 6);
-    const lineNumber = str.substring(6, 10);
+  // Extract the area code, central office code, and line number
+  const areaCode = str.substring(0, 3);
+  const centralOfficeCode = str.substring(3, 6);
+  const lineNumber = str.substring(6, 10);
 
-    // Return the formatted phone number
-    return `(${areaCode}) ${centralOfficeCode}-${lineNumber}`;
+  // Return the formatted phone number
+  return `(${areaCode}) ${centralOfficeCode}-${lineNumber}`;
 }
 
 // Test the function
 const phoneNumber = data[0].phone;
 console.log('Phone Number:', formatPhoneNumber(phoneNumber));
-
